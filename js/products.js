33b6466354827075c075cd0e199ddc7572ed2c9d
jonnebonde <jonnebonde@gmail.com>
2022-01-22 23:16:46 +0100

    enter

--- js/products.js
+++ js/products.js
@@ -14,3 +14,41 @@ const products = [
         soldOut: true,
     },
 ];
+
+
+const container = document.querySelector(".container");
+
+console.log(container)
+
+let html = "";
+
+
+
+
+for(let i = 0; i < products.length; i++) {
+    console.log(products[i].name)
+
+    let stock = products[i].soldOut;
+  
+
+    if(stock === false) {
+        stock = "green";
+    }
+    else {
+        stock = "red";
+    }
+    
+    let price = products[i].price;
+
+    if(!price) {
+        price = "Unknown";
+    }
+    
+    
+    html += `<h1 style="color: ${stock}">${products[i].name}</h1>
+            <p>Price: ${price}</p>`;
+
+}
+
+
+container.innerHTML = html;
