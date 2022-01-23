653c0db8ab0af748964d594ab48b3406e7afd14e
jonnebonde <jonnebonde@gmail.com>
2022-01-23 03:55:42 +0100

    enter

--- js/product.js
+++ js/product.js
@@ -3,3 +3,22 @@ const product = {
     price: 99,
     onSale: true,
 };
+
+const container = document.querySelector(".container");
+
+let price = "unavailable";
+
+if(product.price) {
+    price = product.price;
+}
+
+let onSale = "none";
+
+if(product.onSale) {
+    onSale = "underline";
+}
+
+container.innerHTML = ` <div class="product">
+                        <h4 style="text-decoration: ${onSale}">${product.name}</h4>
+                        <div>Price: ${price}</div>
+                        </div>`;
